b7281045cac99e328d231ec99e8ecc3ca09b7761
behruz77731 <sekrtubehruz@gmail.com>
2025-04-24 16:59:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = `behruz`;
+console.log(typeof(5555));
+
+let box = prompt (`ismingizni kiriting`)
+
+let box1 = prompt(`Space idingizni kiriting`)
+
+let box2 = prompt(`Space passwordingzini kiriting`)
+
+alert(` name ${box} id  ${box1}  password ${box2} `)
